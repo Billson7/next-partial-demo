eadb65b81df2d665be937d6e0b6ed7a6fbc643bb
Alex Billson <alex.billson@sky.uk>
2024-10-29 17:03:34 +0000

    match loading skeleton to component

--- src/app/ui/planet-details.tsx
+++ src/app/ui/planet-details.tsx
@@ -1,8 +1,10 @@
 import React, { type FC } from "react";
 import { getPlanetDetails } from "../utils/get-planet-details";
+import { Card, CardBody, Divider, Spacer } from "@nextui-org/react";
 
 type Planets = {
   name: string;
+  population: string;
 };
 
 export const PlanetDetails: FC = async () => {
@@ -12,7 +14,14 @@ export const PlanetDetails: FC = async () => {
     <div>
       {data.results.map((planet: Planets) => (
         <div key={planet.name}>
-          <h2>{planet.name}</h2>
+          <Card>
+            <CardBody>
+              <h2>{planet.name}</h2>
+              <h2>Population: {planet.population}</h2>
+            </CardBody>
+          </Card>
+          <Divider className="my-4" />
+          <Spacer x={4} />
         </div>
       ))}
     </div>
