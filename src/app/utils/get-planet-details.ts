06f9bd22cb86eee32e7a17062863f38c84540353
Alex Billson <alex.billson@sky.uk>
2024-10-29 10:56:52 +0000

    remove use cache from the incorrect hook

--- src/app/utils/get-planet-details.ts
+++ src/app/utils/get-planet-details.ts
@@ -1,7 +1,5 @@
 import { API_URL } from "../constants";
 
 export const getPlanetDetails = async () => {
-  "use cache";
-
   return await fetch(`${API_URL}planets`).then((data) => data.json());
 };
